39424a732ee035b8e0e0d962b07fd9b0b86ece19
anulmehta48 <anulmehta1848@gmail.com>
2024-05-21 17:15:20 +0530

    s

--- 02_DataTypes/02_Array/01_Array.js
+++ 02_DataTypes/02_Array/01_Array.js
@@ -1,16 +1,10 @@
-let a=10 // this is scalar variable or  single valued variable
+let a = 10; // this is scalar variable or  single valued variable
 
-let b=[2,5,2,5,8,9,10] // this is vector or array or multiple valued variable
+let b = [2, 5, 2, 5, 8, 9, 10]; // this is vector or array or multiple valued variable
 
 //Array=> Array is collection of elements that may be same or may not be same elements.
-//        Array is ordered collection data and element stored based on index 
+//        Array is ordered collection data and element stored based on index
 //        In javascript gives a flexibilty that you can store multiple type of data in one array.
-
-// Array is mutable
-// let arr=[2,5,9,6,3,7,5,2]
-// console.log(arr);
-// arr[5]=22
-// console.log(arr);
 
 //Display or print
 // let arr=[2,5,9,6,3,7,5,2]
@@ -18,17 +12,65 @@ let b=[2,5,2,5,8,9,10] // this is vector or array or multiple valued variable
 // for(let i=0;i<arr.length;i++){
 //     console.log(arr[i]);
 // }
-
-//Instert/push => Push is add one or more element in end of the array and it returns new length of array
+// ! *****************************CREATE*******************************
+//Instert/push/append => Push is add one or more element in end of the array and it returns new length of array (Original)
 // let arr=[1,2,5,3,5,8]
 // let s=arr.push(10)
 // console.log(s);
 // console.log(arr);
 
-// arr.push(5,52,42)// multiple element at one time 
+// arr.push(5,52,42)// multiple element at one time
 // console.log(arr);
 
-//
+//! ***************************READ************************************ 
+
+// Array is mutable
+// let arr=[2,5,9,6,3,7,5,2]
+// console.log(arr);
+// arr[5]=22
+// console.log(arr);
+
+//! *****************************Update******************************** 
+
+//splice=> splice is used for three work add,delete,update in original array and always return deleted element(original)
+// let arr=[1,2,3,4,5]
+// let result=arr.splice(2,2) 
+// console.log(result);
+// console.log(arr);
+
+// let result2=arr.splice(2,2,10) 
+// console.log(result2);
+// console.log(arr);
+
+//unshift => unshift is used to add one or more element in front or start of array and it returns new length of array(Original)
+// let arr=[1,2,3,4,5]
+// let result=arr.unshift(5,2,6)
+// console.log(result);
+// console.log(arr);
 
 
+//! ************************************DELETE******************************
 
+//Delete/popn => pop is removed only last one element from arrat and returned removed elemet of array.(Original)
+// let arr=[1,2,3,4,5]
+// let as=arr.pop()
+// console.log(as);
+// console.log(arr);
+
+//slice => slice is used to remove element and add in new array but not removed from original array(Duplicate)
+//          slice also used nagative index also
+// let arr=[1,2,3,4,5]
+// let result=arr.slice(2)
+// console.log(result);
+// console.log(arr)
+
+// let result2=arr.slice(-2)
+// console.log(result2);
+// console.log(arr)
+
+
+//shift =>shift is used to delete only first one element from array and return deleted element.(Original)
+// let arr=[1,2,3,4,5]
+// let result=arr.shift()
+// console.log(result);
+// console.log(arr);
---
+++ 02_DataTypes/02_Array/LinearSearch.js
@@ -0,0 +1,91 @@
+// let arr=[1,2,3,4,,8,6,4,41]
+// let key=5
+// function LinearSearch(arr,key){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]==key){
+//             return i
+//         }
+//     }
+//     return -1
+// }
+// console.log(LinearSearch(arr,key));
+//TC - O(N)
+//SC - O(1)
+
+// Tansposition Linear serach
+// In this we swap the arr[i] value with arr[i-1] that means privioues one so next time our search will fast
+// let arr=[1,2,3,4,,8,6,4,5]
+// let key=5
+// function LinearSearch(arr,key){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]==key){
+//             let temp=arr[i]
+//             arr[i]=arr[i-1]
+//             arr[i-1]=temp;
+//             return i-1
+//         }
+//     }
+//     return -1
+// }
+// console.log(LinearSearch(arr,key));
+// TC - O(N)
+// SC - O(1)
+
+
+//Move to front/Head
+
+let arr=[1,2,3,4,7,8,6,4,5]
+let key=5
+function LinearSearch(arr,key){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==key){
+            let temp=arr[i]
+            arr[i]=arr[0]
+            arr[0]=temp;
+            return arr[0]
+        }
+    }
+    return -1
+}
+console.log(LinearSearch(arr,key));
+
+
+
+
+
+// Methods 
+// 1. at => It is use to find the element using value of index of array it return the element and it has negative index value also.
+//          if you know index then return value otherwise it return undefined
+// let arr=[1,2,3,4,5]
+// let result=arr.at(3)
+// console.log(result);
+// let result2=arr.at(-3)
+// console.log(result2);
+
+// 2.find => find is used to itrate and find the compaired values and return that value if it true otherwise return undefined 
+//            It is same like filter but only diffrece is it not return array of element
+// let arr=[1,2,3,4,5]
+// let result=arr.find((ele)=>ele>4)
+// console.log(result);
+// console.log(arr);
+
+// 3.findIndex => findIndex take a function and whichever value is return from function that value of index return by the findIndex
+//                  and it return only first element index of array if not then -1 return
+// let arr=[1,2,3,4,5]
+// function large(e){
+//     return e>5
+// }
+// let result=arr.findIndex(large)
+// console.log(result);
+
+// 4.indexOf => indexOf is use for find the index of element and if not then return -1 
+// let arr=[1,2,3,4,4,5]
+// let result=arr.indexOf(9)
+// console.log(result);
+
+// filter => filter is use for compare the values and return the values in new array and if not then empty array []
+// let arr=[1,2,3,4,5]
+// let result=arr.filter((e)=>e>5)
+// console.log(result);
+
+//

11f673c90505159e89cb8abf82ff463d1c75db38
anulmehta48 <anulmehta1848@gmail.com>
2024-05-21 11:01:34 +0530

    D

--- 02_DataTypes/02_Array/01_Array.js
+++ 02_DataTypes/02_Array/01_Array.js
@@ -6,16 +6,29 @@ let b=[2,5,2,5,8,9,10] // this is vector or array or multiple valued variable
 //        Array is ordered collection data and element stored based on index 
 //        In javascript gives a flexibilty that you can store multiple type of data in one array.
 
-
-let arr=[2,5,9,6,3,7,5,2]
-console.log(arr);
-arr[5]=22
-console.log(arr);
-
-
+// Array is mutable
 // let arr=[2,5,9,6,3,7,5,2]
+// console.log(arr);
+// arr[5]=22
+// console.log(arr);
+
+//Display or print
+// let arr=[2,5,9,6,3,7,5,2]
+// console.log(arr[0]);
 // for(let i=0;i<arr.length;i++){
 //     console.log(arr[i]);
 // }
 
+//Instert/push => Push is add one or more element in end of the array and it returns new length of array
+// let arr=[1,2,5,3,5,8]
+// let s=arr.push(10)
+// console.log(s);
+// console.log(arr);
+
+// arr.push(5,52,42)// multiple element at one time 
+// console.log(arr);
+
 //
+
+
+
